6b13ebd14ccb070704b1018fe1623919983846fc
elisandra-soares <flavios.02es@gmail.com>
2023-09-01 16:13:12 -0300

    Adicionei

--- Desfio de Projeto/index.js
+++ Desfio de Projeto/index.js
@@ -1,1 +1,28 @@
-console.log("Deus é real");
+let nomeDoHeroi = "Elisandra";
+let quantidadeXp = 8.000;
+
+if (quantidadeXp < 1.000) {
+    console.log(nomeDoHeroi + ", você é de Ferro!");
+}
+else if(quantidadeXp >= 1.001 && quantidadeXp <= 2.000) {
+    console.log(nomeDoHeroi + ", você é de Bronze!");
+}
+else if(quantidadeXp >= 2.001 && quantidadeXp <= 5.000) {
+    console.log(nomeDoHeroi + ", você é de Prata!");
+}
+else if(quantidadeXp >= 6.001 && quantidadeXp <= 7.000) {
+    console.log(nomeDoHeroi + ", você é de Ouro!");
+}
+else if(quantidadeXp >= 7.001 && quantidadeXp <= 8.000) {
+    console.log(nomeDoHeroi + ", você é de Platina!");
+}
+else if(quantidadeXp >= 8.001 && quantidadeXp <= 9.000) {
+    console.log(nomeDoHeroi + ", você é Ascendete!");
+}
+else if(quantidadeXp >= 9.001 && quantidadeXp <= 10.000) {
+    console.log(nomeDoHeroi + ", você é Imortal!");
+}
+else ("Radiante!");
+
+console.log("O herói de nome " + nomeDoHeroi + " está no nível  " + quantidadeXp);
+
